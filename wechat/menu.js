ce933e79c3de55f93d6c5f1d7190e16d1cbc8021
在我伞下 <swiperyeah.net>
2023-06-23 17:54:27 +0800

    nine commit

--- wechat/menu.js
+++ wechat/menu.js
@@ -7,43 +7,14 @@ module.exports = {
             "url":"http://www.myyaya.com.cn/getCode"
         },
         {
-            "name":"信息查询",
-            "sub_button": [
-                {
-                    "type": 'click',
-                    "name": '查寻成绩',
-                    "key": 'SCORE_QUERY',
-                },
-                {
-                    "type": 'click',
-                    "name": '查寻课表',
-                    "key": 'COURSE_SCHEDULE',
-                },
-            ],
+            "type":"view",
+            "name":"登录",
+            "url":"http://www.myyaya.com.cn/getCode"
         },
         {
-            "name": "发图",
-            "sub_button": [
-                {
-                    "type": "pic_sysphoto",
-                    "name": "系统拍照发图",
-                    "key": "系统拍照发图"
-                },
-                {
-                    "type": "pic_photo_or_album",
-                    "name": "拍照或者相册发图",
-                    "key": "拍照或者相册发图"
-                },
-                {
-                    "type": "pic_weixin",
-                    "name": "微信相册发图",
-                    "key": "微信相册发图"
-                },
-                {
-                    "name": "发送位置",
-                    "type": "location_select",
-                    "key": "rselfmenu_2_0"
-                }]
+            "type":"view",
+            "name":"接口文档",
+            "url":"http://www.myyaya.com.cn/share"
         },
     ]
 }
